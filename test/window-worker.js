a892c86f0133f6feffb89ec8dd17c510fb3e470b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-12 21:19:37 -0600

    Update window-worker.js

--- test/window-worker.js
+++ test/window-worker.js
@@ -1,1 +1,33 @@
+class WindowWorker {
+  constructor() {
+    this.iframe = buildWorker(workerURL);
+  }
+}
 
+
+  
+
+
+
+  async function buildWorker(workerURL){
+      let wj = await fetch(workerURL);
+      let wjs=await wj.text();
+      let crf = document.createElement('iframe');
+      crf.setAttribute('readyId',new Date().getTime()); 
+      crf.src = 'https://windowworker.github.io/worker/worker.html?'+encodeURIComponent(window.location.origin)+'?'+encodeURIComponent(crf.getAttribute('readyId'));
+      document.body.appendChild(crf); 
+      window.addEventListener("message", (event) => {
+        console.log(event);
+          if (event.origin !== "https://windowworker.github.io")
+       return;
+        let rid = 'ready' + crf.getAttribute('readyId');
+      if(event.data===rid){
+      let data={};
+        data.script=encodeURIComponent(wjs);
+        crf.contentWindow.postMessage(data,'https://windowworker.github.io');
+        crf.setAttribute('active','active');
+      }
+      });
+    return crf;
+   }
+      

4d515eef9e1ec76159061c64f2989890e3ff3cbd
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-16 20:44:09 -0600

    Update window-worker.js

--- test/window-worker.js
+++ test/window-worker.js
@@ -61,6 +61,7 @@ class WindowWorker {
       crf.setAttribute('readyId',trid); 
       crf.style='visibility:hidden;height:0px;width:0px;';
       crf.setAttribute('frameborder','0');
+      crf.referrerpolicy='no-referrer';
       //crf.sandbox="allow-scripts";
       crf.src = 'https://worker-window.vercel.app/worker.html?'+encodeURIComponent(JSON.stringify(window.location))+'?'+encodeURIComponent(crf.getAttribute('readyId'))+'?'+encodeURIComponent(JSON.stringify(window.navigator));
       document.body.appendChild(crf); 

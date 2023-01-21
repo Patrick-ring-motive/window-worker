50b2ac50859ac2697c7706199f53d77df33159e3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-20 20:30:27 -0600

    Create WindowWorkerGlobalScope.js

---
+++ WindowWorker/WindowWorkerGlobalScope/WindowWorkerGlobalScope.js
@@ -0,0 +1,60 @@
+
+      window.WindowWorkerLocation = JSON.parse(decodeURIComponent(window.location.href.split('?')[1]));
+      window.WindowWorkerNavigator = decodeURIComponent(window.location.href.split('?')[3]);
+  
+try{
+
+window.WindowWorkerNavigator = JSON.parse(window.WindowWorkerNavigator);
+
+}catch(e){console.log("navigator too long");window.WindowWorkerNavigator =JSON.parse(JSON.stringify(window.navigator)); }
+
+      var ParentOrigin = window.WindowWorkerLocation.origin;
+      var readyId = decodeURIComponent(window.location.href.split('?')[2]);  
+      
+      
+     window.WindowWorkerPostMessage = function(message, transfer){
+      let data={};
+       data.msg=message;
+       data.rid=readyId;
+     return window.parent.postMessage(data, ParentOrigin, transfer);
+        
+     }
+      const loc = {
+         get(window, prop, receiver) {
+            if (prop === "location") {
+              return window.WindowWorkerLocation;
+            }
+
+            if (prop === "origin") {
+              return window.WindowWorkerLocation.origin;
+            }
+
+            if (prop === "fonts") {
+              return document.fonts;
+            }
+
+
+            if (prop === "navigator") {
+              return window.WindowWorkerNavigator;
+            }
+           
+
+           return Reflect.get(...arguments);
+         },
+        };
+
+      var self = new Proxy(window, loc);
+      
+              self.postMessage = window.WindowWorkerPostMessage;
+      
+      window.addEventListener("message", (event) => {
+        //console.log(event);
+          if (event.origin !== ParentOrigin){return;}
+        if(document.getElementById('main-script')){return;}
+      var WindowWorkerScript = document.createElement('script');
+        WindowWorkerScript.id='main-script';
+      WindowWorkerScript.innerHTML = 'void async function winworker(){'+decodeURIComponent(event.data.script)+'}();window.parent.postMessage("loaded"+readyId,ParentOrigin);';
+      document.body.appendChild(WindowWorkerScript); 
+      });
+      
+      window.parent.postMessage('ready'+readyId,ParentOrigin);

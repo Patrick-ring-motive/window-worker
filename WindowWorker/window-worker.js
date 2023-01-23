44d280286cb07768c0e9ecae9e0138d330cb8123
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-22 20:57:29 -0600

    Update window-worker.js

--- WindowWorker/window-worker.js
+++ WindowWorker/window-worker.js
@@ -2,8 +2,9 @@ if(!window.WindowWorkerURL){window.WindowWorkerURL='https://worker-window.vercel
 window.WindowWorkerEvents = new Map();
 window.addEventListener("message", function(e) {
   if (e.data.rid) {
-    let edr = e.data.WindowWorkerId;
-    let funs = window.WindowWorkerEvents.get(edr);
+    let edw = e.data.WindowWorkerId;
+    delete e.data.WindowWorkerId;
+    let funs = window.WindowWorkerEvents.get(edw);
     const funs_length = funs.length;
     for (let i = 0; i < funs_length; i++) {
       try {

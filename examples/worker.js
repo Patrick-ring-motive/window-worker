d3dc0f58ad082af709c73f25a163d5cfb77be194
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-22 20:19:11 -0600

    Create worker.js

---
+++ examples/worker.js
@@ -0,0 +1,11 @@
+self.onmessage = function(e) {
+  console.log('Worker: Message received from main script');
+  const result = e.data[0] * e.data[1];
+  if (isNaN(result)) {
+    self.postMessage('Please write two numbers');
+  } else {
+    const workerResult = 'Result: ' + result;
+    console.log('Worker: Posting message back to main script');
+    self.postMessage(workerResult);
+  }
+}
